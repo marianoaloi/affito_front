ce1812c2b090719ad0b9741a38c3ab808eff88ea
Mariano Aloi <mariano@aloi.com.br>
2025-07-25 14:03:58 +0200

    Work in progress: Map page and features table updates

---
+++ src/app/map/Map.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+
+export const Photos = styled.div`
+  display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-content: space-between;
+    justify-content: space-between;   
+  `
+export const Photo = styled.img`
+  width  height: 80px;
+    `
--- src/app/map/Map.tsx
+++ src/app/map/Map.tsx
@@ -1,50 +1,51 @@
-'use client'
-
-
-
-// It's necessary to import the marker icon images
-// import icon from 'leaflet/dist/images/marker-icon.png';
-// import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import { useEffect } from 'react';
 import { AffitiPageProps } from '../entity/AffitiPageProps';
+import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import { Photo, Photos } from './Map.styled';
+let DefaultIcon;
 
-let DefaultIcon ;
 
-useEffect(() => {
-  
-  L.Marker.prototype.options.icon = new L.Icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
-}, []);
-
-export const Map : React.FC<AffitiPageProps> = ({ affiti }) => {
+export const Map: React.FC<AffitiPageProps> = ({ affiti }) => {
   const position: [number, number] = [46.0689, 13.2224];
 
-  return (<>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: 400, width: "100%"}}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {affiti.map((affito) => {
-          const lat = affito.realEstate.properties[0].location.latitude;
-          const lon = affito.realEstate.properties[0].location.longitude;
-          return (
-            <Marker key={affito.realEstate.id} position={[lat, lon]} >
-              <Popup>
-                {affito.realEstate.title}
-              </Popup>
-            </Marker>
-          )
-        })}
-      </MapContainer> 
-    </>
+
+  useEffect(() => {
+    const L = require('leaflet');
+    L.Marker.prototype.options.icon = new L.Icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+  }, []);
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />      {affiti.map((affito) => {
+        const propt = affito.realEstate.properties[0];
+        const { latitude, longitude } = propt.location;
+        return (
+          <Marker key={affito.realEstate.id} position={[latitude, longitude]}>
+            <Popup>
+              <div>
+                <h2>{affito.realEstate.price.formattedValue}</h2>
+                <p>{affito.realEstate.title}</p>
+                <Photos>
+                  {propt.multimedia.photos.map((photo, index) => (
+                    <Photo key={index} src={photo.urls.small} alt={`Photo ${index + 1}`}  />
+                  ))}
+                </Photos>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
   )
 }
